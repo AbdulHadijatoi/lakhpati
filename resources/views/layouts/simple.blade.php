cfde3d9da7301adc05da944a5c45fa156809e420
H∆DI <hadi@thiqatech.com>
2024-03-18 16:30:02 +0400

    updated layout content

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -4,11 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-        <title>OneUI - Bootstrap 5 Admin Template &amp; UI Framework</title>
+        <title>{{$page_title??"Admin Panel Lakhpati"}}</title>
 
-        <meta name="description" content="OneUI - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
-        <meta name="author" content="pixelcave">
-        <meta name="robots" content="noindex, nofollow">
+        <meta name="author" content="abdul hadi">
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
