b06ed9e9933cc13875b46a863d7ddd06ace66937
Abdul Hadi <Abdulhadijatoi@gmail.com>
2024-10-03 12:02:25 +0500

    added participat in contest api

--- app/Http/Controllers/API/ContestController.php
+++ app/Http/Controllers/API/ContestController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\AppBaseController;
 use App\Models\Comuna;
 use App\Models\Contest;
+use App\Models\Participant;
 use App\Models\Population;
 use App\Models\Sector;
 use App\Models\TypeOfFault;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Request;
 
 class ContestController extends AppBaseController {
     
@@ -27,5 +28,31 @@ class ContestController extends AppBaseController {
 
         return $this->sendDataResponse($data);
     }
+    
+    public function participate(Request $request) {
+        $request->validate([
+            'contest_id' => 'required|exists:contests,id'
+        ]);
+    
+        $user = Auth::user();
+    
+        // Check if the user has already participated in the contest
+        $existingParticipation = Participant::where('user_id', $user->id)
+                                            ->where('contest_id', $request->contest_id)
+                                            ->first();
+    
+        if ($existingParticipation) {
+            return $this->sendError('You have already participated in this contest.');
+        }
+    
+        // Create a new participation record
+        Participant::create([
+            'user_id' => $user->id,
+            'contest_id' => $request->contest_id,
+        ]);
+    
+        return $this->sendSuccess('Success!');
+    }
+    
     
 }
--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,7 @@ Route::group(['prefix' => 'v1'], function() {
         });
 
         Route::get('/contests', [ContestController::class, 'userContests']);
+        Route::post('/participate', [ContestController::class, 'participate']);
     });
 
     // Contest-related routes
